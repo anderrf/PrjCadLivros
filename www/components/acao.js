07b40d51313f5ea307e9297c7370808cb0efdd3a
anderrf <andersonrochafarias@gmail.com>
2019-08-06 03:26:41 +0900

    fim do CRUD básico

--- www/components/acao.js
+++ www/components/acao.js
@@ -40,6 +40,7 @@ $(document).on("click", "#btnSalvar", function(){
 });
 
 $(document).on("click", "#btnCancelar", function(){
+  $("#codigo").val("");
   $("#titulo").val("");
   $("#autor").val("");
   $("#ano").val("");
@@ -63,7 +64,7 @@ function lista(){
         success: function(data){
             var itemlista = "";
             $.each(data.livro,function(i,dados){
-              itemlista += "<option value='"+dados.codigo+"'>"+dados.nome+"</option>"; 
+              itemlista += "<option value='"+dados.codigo+"'>"+dados.titulo+"</option>"; 
             });
         $("#lista").html(itemlista);
         },
@@ -73,3 +74,72 @@ function lista(){
         }
     });    
 };
+
+$(document).on("change", "#lista", function(){
+  var codigoEscolhido = $("option:selected", ("#lista")).val();
+  $.ajax({
+    type: "post", //como enviar
+    url: "https://cadlivros.000webhostapp.com/lista-um.php", //para onde enviar
+    data: "codigo="+codigoEscolhido,
+    dataType:"json",
+    //se der certo
+    success: function(data){
+      $("#codigo").val(data.livro.codigo);
+      $("#titulo").val(data.livro.titulo);
+      $("#autor").val(data.livro.autor);
+      $("#ano").val(data.livro.ano);
+    },
+    //se der errado
+    error: function(data){
+      navigator.notification.alert(data);
+    }
+  });
+});
+
+$(document).on("click", "#btnSalvarAlterar", function(){
+  var parametros = {
+    "codigo": $("#codigo").val(),
+    "titulo": $("#titulo").val(),
+    "autor": $("#autor").val(),
+    "ano": $("#ano").val()
+  };
+
+  $.ajax({
+    type: "post", //como enviar
+    url: "https://cadlivros.000webhostapp.com/alterar.php", //para onde enviar
+    data: parametros,
+    dataType:"json",
+    //se der certo
+    success: function(data){
+      navigator.notification.alert(data);
+      desabilita();
+      document.location.reload(true);
+    },
+    //se der errado
+    error: function(data){
+      navigator.notification.alert(data);
+    }
+  });
+});
+
+$(document).on("click", "#btnExcluir", function(){
+  var codigoEscolhido = $("#codigo").val();
+  $.ajax({
+    type: "post",
+    url: "https://cadlivros.000webhostapp.com/excluir.php",
+    data: "codigo="+codigoEscolhido,
+    dataType: "json",
+    success: function(data){
+      navigator.notification.alert(data);
+      $("#codigo").val("");
+      $("#titulo").val("");
+      $("#autor").val("");
+      $("#ano").val("");
+      desabilita();
+      document.location.reload(true);
+    },
+    error: function(data){
+      navigator.notification.alert(data);
+    }
+  })
+});
